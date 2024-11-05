da0bb6285b69de696fe0c115b02da3c9637be670
Dorian Cottle <dorian.cottle@students.snow.edu>
2024-11-05 10:21:46 -0700

    added login stuff

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
+
+const uri = import.meta.env.VITE_REDIRECT_URI;
+ 
+if (!uri) console.log("REDIRECT_URI is not set");
+else console.log("REDIRECT_URI is: " +  uri );
+
+const oidcConfig: AuthProviderProps = {
+  authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
+  client_id: "dorian-demo2",
+  redirect_uri: `${uri}`,
+  onSigninCallback: async (user) => {
+    console.log("called back!");
+    const newUrl = window.location.href.split('?')[0];
+    window.history.replaceState({}, document.title, newUrl);
+    document.cookie = `jwt_token=${user?.id_token}`;
+  },
+  automaticSilentRenew: true
+  
+};
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
