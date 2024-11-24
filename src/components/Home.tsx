982e97096d63c143f10c1acfde63441a0963d8b8
therealdelorian <thenewdorian21@gmail.com>
2024-11-23 23:03:46 -0700

    had an idea for a new component to work on next time

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,8 +13,11 @@ function App() {
 
   return (
     <>
-      <div className="row justify-content-center "></div>
       <NavBar></NavBar>
+      <div className="container">
+        {/* <UserSummary/> This component will be a little card-like thing that contains streak info, last temple visited, maybe upcoming visits, etc.  */}
+        <button className="btn btn-danger ">Click here to start </button>
+      </div>
       {/*<button className="btn btn-danger" onClick={throwError}>Throw Error</button>*/}
     </>
   );
